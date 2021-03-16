bcc2d6ee9a092647fffcbf62fa848a4b2d819644
Andrew Nguyen <an2eqc@virginia.edu>
2021-03-15 20:04:59 -0400

    updated settings.py with static stuff, added static folder to open_now app

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 LOGIN_REDIRECT_URL = '/open_now/home'
 
 django_heroku.settings(locals())
