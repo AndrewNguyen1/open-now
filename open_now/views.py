450f0a4375170c2947cd494eb3a1dd9e5767aba0
Alex White <67017834+adw5ke@users.noreply.github.com>
2021-04-05 18:06:16 -0400

    map integration

--- open_now/views.py
+++ open_now/views.py
@@ -7,7 +7,14 @@ from django.template import loader
 
 from django.views import generic
 
-from .models import Login
+from .models import Login, Measurement
+from .forms import MeasurementModelForm
+
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from .utils import get_geo, get_center_coordinates, get_zoom, get_ip_address
+import folium
+
 
 
 def login(request):
@@ -30,3 +37,82 @@ class HomeView(generic.ListView):
         """
         return
 
+def calculate_distance_view(request):
+	# initial values
+	distance = None
+	destination = None
+
+	obj = get_object_or_404(Measurement, id=1)
+	form = MeasurementModelForm(request.POST or None)
+	geolocator = Nominatim(user_agent='open_now')
+
+	# ip address from local host
+	# doesn't work :(
+	ip_ = get_ip_address(request)
+	# print(ip_)
+
+	# hard coded ip address for charlottesville va
+	ip = '128.143.22.119'
+	country, city, lat, lon = get_geo(ip)
+
+	location = geolocator.geocode(city)
+
+	# location coordinates
+	l_lat = lat
+	l_lon = lon
+	pointA = (l_lat, l_lon)
+
+	# initial folium map
+	m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon), zoom_start=8)
+
+	# location marker
+	folium.Marker([l_lat, l_lon], tooltip='click here for more', popup=city['city'], icon=folium.Icon(color='purple')).add_to(m)
+
+	if form.is_valid():
+		instance = form.save(commit=False)
+		destination_ = form.cleaned_data.get('destination')
+		destination = geolocator.geocode(destination_)
+
+		# destination coordinates
+		d_lat = destination.latitude
+		d_lon = destination.longitude
+
+		# print(destination)
+		pointB = (d_lat, d_lon)
+
+		# distance calculation
+		distance = round(geodesic(pointA, pointB).km, 2)
+
+		# map modification
+		m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon, d_lat, d_lon), zoom_start=get_zoom(distance))
+
+		# location marker
+		folium.Marker([l_lat, l_lon], tooltip='click here for more', popup=city['city'], icon=folium.Icon(color='purple')).add_to(m)
+
+		# destination marker
+		# location marker
+		folium.Marker([d_lat, d_lon], tooltip='click here for more', popup=city['city'], icon=folium.Icon(color='red', icon='cloud')).add_to(m)
+
+		# draw the lint between the location and destination
+		line = folium.PolyLine(locations=[pointA, pointB], weight=2, color='blue')
+
+		# add the line to the map
+		m.add_child(line)
+
+
+		instance.location = location
+		instance.distance = distance
+		instance.save()
+
+	m = m._repr_html_()
+	# distance = None
+
+
+	context = {
+		'distance': distance,
+		'destination': destination,
+		'form': form,
+		'map': m,
+	}
+
+	return render(request, 'open_now/main.html', context)
