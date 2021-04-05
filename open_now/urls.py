34cbd3136ac25eeb144f25c69be78c17ce89cc50
Alex White <67017834+adw5ke@users.noreply.github.com>
2021-04-05 18:03:22 -0400

    map integration

--- open_now/urls.py
+++ open_now/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 from . import views
+from .views import calculate_distance_view
 
 app_name = 'open_now'
 
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
     path('home/', views.HomeView.as_view(), name='home'),
+    path('home/map/', calculate_distance_view, name='calculate-view')
+
 
 ]
 
