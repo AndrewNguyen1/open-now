f094136c6b4116bf206a61efb9e463e08d874e66
Alex White <67017834+adw5ke@users.noreply.github.com>
2021-04-19 02:12:19 -0400

    Update admin.py

--- open_now/admin.py
+++ open_now/admin.py
@@ -2,19 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-<<<<<<< Updated upstream
-from .models import Business, Forum, Discussion, Location, Search
-=======
 from .models import *
->>>>>>> Stashed changes
+
 
 admin.site.register(Business)
 admin.site.register(Forum)
 admin.site.register(Discussion)
-<<<<<<< Updated upstream
-=======
 admin.site.register(Review)
 admin.site.register(OpeningHours)
->>>>>>> Stashed changes
 admin.site.register(Location)
 admin.site.register(Search)
