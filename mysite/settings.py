e57d04c3eae2ae2f6efd3dfc8755895c43e497d3
Alex White <67017834+adw5ke@users.noreply.github.com>
2021-04-19 16:28:53 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,6 +146,8 @@ USE_TZ = True
 
 GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
